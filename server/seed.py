1898075dc2b54a3cef49edc4a5b3f2a78e07ae34
MikeUrezzio <mikeurezzio@gmail.com>
2023-08-21 23:12:31 -0400

    create_spells and create_game_classes in seed.py test data

--- server/seed.py
+++ server/seed.py
@@ -20,10 +20,27 @@ if __name__ == "__main__":
             db.session.commit()
         
         def create_spells ():
-            s1 = Spell( name = "Second Wind", )
+            fs1 = Spell ( name = "Second Wind", game_class_id = 1, level = 1 )
+            fs2 = Spell ( name = "Action Surge", game_class_id = 1, level = 2 )
+            ps1 = Spell ( name = "Lay on Hands", game_class_id = 2, level = 1)
+            ps2 = Spell ( name = "Holy Rebuke", game_class_id = 2, level = 1)
+            ps3 = Spell ( name = "Turn Undead", game_class_id = 2, level = 1)
+            ws1 = Spell ( name = "Magic Missles", game_class_id = 3, level = 1 )
+            ws2 = Spell ( name = "Grease", game_class_id = 3, level = 1 )
+            ws3 = Spell ( name = "Witch Bolt", game_class_id = 3, level = 1 )
+            ws4 = Spell ( name = "Shatter", game_class_id = 3, level = 2 )
+            ws5 = Spell ( name = "Scorching Ray", game_class_id = 3, level = 2 )
+            ws6 = Spell ( name = "Misty Step", game_class_id = 3, level = 2 )
+            spells = [ fs1, fs2, ps1, ps2, ps3, ws1, ws2, ws3, ws4, ws5, ws6]
+            for spell in spells:
+                db.session.add ( spells )
+            db.session.commit()
         
-        print ( "Seeding Game Classes" )
+        print ( "Seeding Game Classes..." )
         create_game_class()
+
+        print ( "Seeding Spells...")
+        create_spells()
 
         print ( "Seeing complete.... for now." )
 
