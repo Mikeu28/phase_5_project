9abcff6f1767912bad853858a1124672dcfdc83d
MikeUrezzio <mikeurezzio@gmail.com>
2023-08-21 22:12:04 -0400

    Revised tables and relationships set

--- server/app.py
+++ server/app.py
@@ -1,8 +1,18 @@
 from config import app, db, api
+from flask import request, make_response
+from flask_restful import Resource
+from models import Character#, Classes, Feats
 
 @app.route( "/" )
 def index():
     return '<h1>Phase 5 Project</h1>'
+
+class Characters(Resource):
+
+    def get(self):
+        return make_response([character.to_dict() for character in Character.query.all()], 200)
+
+api.add_resource(Characters, "/characters")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,15 +5,89 @@ from config import db, metadata
 metadata = metadata
 db = db
 
+class User ( db.Model, SerializerMixin ):
+    __tablename__ = "user"
+
+    id = db.Column ( db.Integer, primary_key = True )
+    username = db.Column ( db.String, nullable = False, unique = True)
+    password = db.Column ( db.String, nullable = False )
+    profile_picture = db.Column ( db.String )
+
+    #Relationships
+
+    characters = db.relationship ( "Character", back_populates = "user" )
+
 class Character ( db.Model, SerializerMixin ):
-    __tablename__ = "characters"
+    __tablename__ = "character"
+
+    id = db.Column ( db.Integer, primary_key = True )
+    name = db.Column ( db.String, nullable = False)
+    user_id = db.Column ( db.Integer, db.ForeignKey( "user.id" ) )
+
+    #Relationships
+
+    character_classes = db.relationship ( "Character_Class", back_populates = "character" )
+    game_classes = association_proxy ( "character_class", "game_class" )
+    spell_selections = db.relationship ( "Spell_Selection", back_populates = "character" )
+    user = db.relationship ( "User", back_populates = "characters" )
+
+
+
+class Character_Class ( db.Model, SerializerMixin ):
+    __tablename__ = "character_class"
+
+    id = db.Column ( db.Integer, primary_key = True )
+    classes_id = db.Column ( db.Integer,  db.ForeignKey( "game_class.id" ), nullable = False )
+    character_id = db.Column ( db.Integer,  db.ForeignKey( "character.id" ), nullable = False )
+    level = db.Column ( db.Integer, nullable = False )
+    
+    #Relationships
+
+    character = db.relationship ( "Character", back_populates = "character_classes")
+    game_class = db.relationship ( "Game_Class", back_populates = "character_classes")
+
+class Game_Class ( db.Model, SerializerMixin ):
+    __tablename__ = "game_class"
 
     id = db.Column ( db.Integer, primary_key = True )
     name = db.Column ( db.String, nullable = False )
 
-class Classes ( db.Model, SerializerMixin ):
-    id = db.Column ( db.Integer, primary_key = True)
+    #Relationships
 
-class Feats ( db.Model, SerializerMixin ):
+    character_classes = db.relationship ( "Character_Class", back_populates = "game_class" )
+    characters = association_proxy ( "character_class", "character" )
+
+    
+
+
+class Spell ( db.Model, SerializerMixin ):
+    __tablename__ = "spell"
+
     id = db.Column ( db.Integer, primary_key = True )
+    name = db.Column ( db.String, nullable = False )
+    classes_id = db.Column ( db.Integer, db.ForeignKey( "game_class.id" ), nullable = False )
+    level = db.Column ( db.Integer, nullable = False )
+
+    #Relationships
+
+    spell_selections = db.relationship ( "Spell_Selection", back_populates = "spell" )
+
+
+class Spell_Selection ( db.Model, SerializerMixin ):
+    __tablename__ = "spell_selection"
+
+    id = db.Column ( db.Integer, primary_key = True )
+    character_id = db.Column ( db.Integer, db.ForeignKey( "character.id" ), nullable = False )
+    spells_id = db.Column ( db.Integer, db.ForeignKey( "spell.id" ) )
+
+    #Relationships
+
+    spell = db.relationship ( "Spell", back_populates = "spell_selections" )
+    character = db.relationship ( "Character", back_populates = "spell_selections" )
+
+
+
+
+
+
 
